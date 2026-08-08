4fd62529d28b1a7735ef5210852aad3f6d5f022f
Govind Singh Rathore <govindrathore026@gmail.com>
2026-08-08 09:50:54 +0530

    Time: 76 ms (29.03%) | Memory: 111.3 MB (44.17%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+         for(int num : nums){
+            if(mp.find(num)!=mp.end()){
+                return true ;
+            }
+            mp[num]++;
+         }
+         return false;
+        
+    }
+};
