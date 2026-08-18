a2a2b167dad72192bc96df1a3c1e94bac29997d2
Govind Singh Rathore <govindrathore026@gmail.com>
2026-08-18 11:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (98.19%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+                int n=nums.size();
+        int i=0;
+        while(i<n){
+        
+            int correct= nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums[i],nums[correct]);
+            }else i++;}
+
+            for(int i=0;i<n;i++){
+                if(nums[i]!=i+1){
+                  return{nums[i],i+1};
+                }
+            }
+      return{-1,-1};  
+    }
+};
